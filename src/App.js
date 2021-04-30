dada4b87a553f6c2b3eb5a5f100d0e3d49ac6e65
copetit <copetit412@gmail.com>
2021-04-30 20:51:34 +0900

    useTitle

--- src/App.js
+++ src/App.js
@@ -1,53 +1,22 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import "./styles.css";
 
-const content = [
-  {
-    tab: "Section 1",
-    content: "content of Section 1 "
-  },
-  {
-    tab: "Section 2",
-    content: "content of Section 2 "
-  }
-];
-
-const useTabs = (initialTab, allTabs) => {
-  if (!allTabs || !Array.isArray(allTabs)) {
-    return;
-  }
-  // 3. setCurrentIndex -> useState -> currentIndex -> current Item 변화
-  const [currentIndex, setCurrentIndex] = useState(initialTab);
-  console.log("fuga");
-  console.log(currentIndex);
-  console.log(setCurrentIndex);
-  return {
-    currentItem: allTabs[currentIndex],
-    // 2. changeItem -> setCurrentIndex로 보냄
-    changeItem: setCurrentIndex
+const useTitle = (initinalTitle) => {
+  const [title, setTitle] = useState(initinalTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
   };
+  useEffect(updateTitle, [title]);
+  return setTitle;
 };
-
 export default function App() {
-  // returnのobject形だからこうかいている
-  const { currentItem, changeItem } = useTabs(0, content);
-  console.log("hoge");
-  console.log(currentItem);
-  console.log(changeItem);
-  // const currentItem = useTabs(0, content).currentItem
-  // const changeItem = useTabs(0, content).changeItem
-
+  // 인수로 받은 문자열으로 setTitle return 한다
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
-      {/* map(value, index, array) */}
-      {/* 「value」は、配列の値  */}
-      {/* 「index」は、配列のインデックス番号 */}
-      {/* 「array」は、現在処理している配列 */}
-      {content.map((section, index) => (
-        //1. button을 누르면 changeItem에 인덱스번호 (0 or 1)를 보냄
-        <button onClick={() => changeItem(index)}>{section.tab} </button>
-      ))}
-      <div>{currentItem.content}</div>
+      <div>hi</div>
     </div>
   );
 }
