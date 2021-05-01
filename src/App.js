4835d380dedfa6eadb33ef505d7cb8c34eda11b7
copetit <copetit412@gmail.com>
2021-05-01 21:31:38 +0900

    onConfirm

--- src/App.js
+++ src/App.js
@@ -1,38 +1,30 @@
 import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
-const useClick = (onClick) => {
-  if (typeof onClick !== "function") {
+const useConfirm = (message = "", onConfirm, onCancel) => {
+  if (onConfirm && typeof onConfirm !== "function") {
     return;
   }
-  const el = useRef();
-
-  // only componentDidMonut
-  useEffect(() => {
-    // useEffect내에 있는 func은 componentDidMonut, componentDidUpdate때 호출됨
-    if (el.current) {
-      el.current.addEventListener("click", onClick);
+  if (onCancel && typeof onCancel !== "function") {
+    return;
+  }
+  const confirmAction = () => {
+    if (confirm(message)) {
+      onConfirm();
+    } else {
+      onCancel();
     }
-    // componentWillUnmount 때 호출됨
-    return () => {
-      if (el.current) {
-        el.current.removeEventListener("click", onClick);
-      }
-    };
-    // no dependecy = update는 고려하지 않음
-  }, []);
-  return el;
+  };
+  return confirmAction;
 };
 
 export default function App() {
-  const sayHello = () => console.log("hello");
-  const title = useClick(sayHello);
-  // const input = useRef();
-  // setTimeout(() => input.current.focus(), 3000);
+  const deleteWorld = () => console.log("Deleting the world");
+  const abort = () => console.log("Aborted");
+  const confirmDelete = useConfirm("Are you sure?", deleteWorld, abort);
   return (
     <div className="App">
-      <h1 ref={title}>hi</h1>
-      {/* <input ref={input} placeholder="la" /> */}
+      <button onClick={confirmDelete}>Delete the world</button>
     </div>
   );
 }
