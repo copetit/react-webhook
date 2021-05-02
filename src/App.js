e05f8d4e3abe5c005fd76dec7fc547858c98e587
copetit <copetit412@gmail.com>
2021-05-02 18:35:24 +0900

    useBeforeLeave

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import { useState, useEffect, useRef } from "react";
 import "./styles.css";
 
-const usePreventLeave = () => {
-  const listener = (event) => {
-    event.preventDefault();
-    event.returnValue = "";
+const useBeforeLeave = (onBefore) => {
+  if (typeof onBefore !== "function") {
+    return;
+  }
+  const handle = (event) => {
+    // event안에는 여러 obj가 있다.
+    // clientY를 사용해서 높이를 체크/ 화면 위로 가면 onBefore이 실행되게 함
+    const { clientY } = event;
+    if (clientY <= 0) {
+      onBefore();
+    }
   };
-  // beforeupload는 window가 닫히기전에 function을 실행시킨다
-  const enablePrevent = () => addEventListener("beforeunload", listener);
-  const disablePrevent = () => removeEventListener("beforeunload", listener);
-  return { enablePrevent, disablePrevent };
+  useEffect(() => {
+    document.addEventListener("mouseleave", handle);
+    return () => {
+      document.removeEventListener("mouseleave", handle);
+    };
+  }, []);
 };
-
 export default function App() {
-  const { enablePrevent, disablePrevent } = usePreventLeave();
+  const begForLife = () => console.log("pls dont leave");
+  useBeforeLeave(begForLife);
   return (
     <div className="App">
-      <button onClick={enablePrevent}>Protect</button>
-      <button onClick={disablePrevent}>Unrotect</button>
+      <h1>Hello</h1>
     </div>
   );
 }
