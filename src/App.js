83a4e81328949a84798035189071e6584b71b284
copetit <copetit412@gmail.com>
2021-05-02 19:34:59 +0900

    처리 순서 추가

--- src/App.js
+++ src/App.js
@@ -5,22 +5,26 @@ const useBeforeLeave = (onBefore) => {
   if (typeof onBefore !== "function") {
     return;
   }
+  // 이벤트가 발생할때 (mouseleave)불려짐 (3)
   const handle = (event) => {
     // event안에는 여러 obj가 있다.
-    // clientY를 사용해서 높이를 체크/ 화면 위로 가면 onBefore이 실행되게 함
+    // clientY를 사용해서 높이를 체크/ 화면 위로 가면 onBefore이 실행되게 함 (4)
     const { clientY } = event;
     if (clientY <= 0) {
       onBefore();
     }
   };
   useEffect(() => {
+    // dom이 생성될때 한번 불려짐 (2)
     document.addEventListener("mouseleave", handle);
+    console.log("2");
     return () => {
       document.removeEventListener("mouseleave", handle);
     };
   }, []);
 };
 export default function App() {
+  // 1
   const begForLife = () => console.log("pls dont leave");
   useBeforeLeave(begForLife);
   return (
