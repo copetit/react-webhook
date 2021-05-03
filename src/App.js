b17f93410be8ae9e45af64626f5f076bfb22fcde
copetit <copetit412@gmail.com>
2021-05-03 22:31:25 +0900

    useNotification

--- src/App.js
+++ src/App.js
@@ -2,37 +2,39 @@ import { useState, useEffect, useRef } from "react";
 import { flushSync } from "react-dom";
 import "./styles.css";
 
-const useFullscreen = (callback) => {
-  const el = useRef();
-
-  const triggerFull = () => {
-    if (el.current) {
-      el.current.requestFullscreen();
-    }
-    if (callback && typeof callback === "function") {
-      callback(true);
+//notification API : https://developer.mozilla.org/ja/docs/Web/API/Notification
+const useNotification = (title, options) => {
+  // window가 아니라면 이 브라우져에서는 notification을 지원하지 않기 때문
+  if (!("Notification" in window)) {
+    return;
+  }
+  const fireNotif = () => {
+    if (Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          new Notification(title, options);
+        } else {
+          // 거절했을때
+          return;
+        }
+      });
+    } else {
+      new Notification(title, options);
     }
   };
-  const exitFull = () => {
-    document.exitFullscreen();
-    if (callback && typeof callback === "function") {
-      callback(false);
-    }
-  };
-  return { el, triggerFull, exitFull };
+  return fireNotif;
 };
+
 export default function App() {
-  const onFullS = (isFull) => {
-    console.log(isFull ? "We are full" : "We are small");
+  const options = {
+    body: "I think cute!",
+    icon: "https://images-fe.ssl-images-amazon.com/images/I/8135hRXxTvL.png"
   };
-  const { el, triggerFull, exitFull } = useFullscreen(onFullS);
+  const triggerNotif = useNotification("redpanda is cute", options);
   return (
     <div className="App" style={{ height: "1000vh" }}>
-      <div ref={el}>
-        <img src="https://images-fe.ssl-images-amazon.com/images/I/8135hRXxTvL.png" />
-        <button onClick={exitFull}>Exit Screen </button>
-      </div>
-      <button onClick={triggerFull}>FULL Screen </button>
+      <h1>Hello</h1>
+      <button onClick={triggerNotif}>Notification</button>
     </div>
   );
 }
